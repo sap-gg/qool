499d4c47ea6e42823bdab1981e0697bdb5e15d69
darmiel <71837281+darmiel@users.noreply.github.com>
2022-10-30 20:21:58 +0100

    fix: wrong command names

--- src/main/java/gg/sap/smp/itemremover/ItemRemover.java
+++ src/main/java/gg/sap/smp/itemremover/ItemRemover.java
@@ -1,5 +1,6 @@
 package gg.sap.smp.itemremover;
 
+import gg.sap.smp.itemremover.commands.TrashCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -9,10 +10,10 @@ public final class ItemRemover extends JavaPlugin {
     @Override
     public void onEnable() {
         final DevNull devNull = new DevNull();
-        Objects.requireNonNull(this.getCommand("trash")).setExecutor(devNull);
+        Objects.requireNonNull(this.getCommand("devnull")).setExecutor(devNull);
         this.getServer().getPluginManager().registerEvents(devNull, this);
 
-        Objects.requireNonNull(this.getCommand("devnull")).setExecutor(new DevNull());
+        Objects.requireNonNull(this.getCommand("trash")).setExecutor(new TrashCommand());
     }
 
 }
